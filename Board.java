9c18558b14fa6837400dfd415cca05f7a97c5089
roc-4001 <166049899+roc-4001@users.noreply.github.com>
2024-11-12 12:44:22 +0000

    Add files via upload

---
+++ Board.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class Board
+{
+    private ArrayList<Pip> Pips = new ArrayList<Pip>();
+    private ArrayList<Pip> Bar = new ArrayList<Pip>(2);
+
+    private final int num_pips = 24;
+
+    private final Player_IDs IDs;
+
+    private Turn turn = new Turn();
+    private ArrayList<Integer> rolls = new ArrayList<Integer>();
+    private Dice dice = new Dice(rolls);
+
+    private Movement movement;
+
+    private Command_Generator genCommands;
+
+
+    public Board()
+    {
+        this.IDs = new Player_IDs(turn, dice);
+        this.Bar.add(new Pip());
+        this.Bar.add(new Pip());
+        generateBoard();
+        movement = new Movement(dice, Pips, turn, Bar);
+    }
+
+    public void generateBoard()
+    {
+
+        int[][] start_matrix = {{0,5,7,11}, {2,5,3,5}};
+        boolean[][] start_colours = { {false, true, true, false}, {true, false, false, true} };
+
+        int offset = 0;
+        if(turn.returnOrientation())
+            offset = 1;
+
+        for(int i =0; i< num_pips; i++)
+            Pips.add(new Pip());
+
+        for(int j=0; j<2; j++)
+            for(int i=0; i< start_matrix[0].length; i++)
+                for(int k=0; k < start_matrix[1][i]; k++)
+                {
+                    int index = (int) (Math.pow(-1,j)*(start_matrix[0][i])+( j*(num_pips-1)));
+                    Pips.get(index).addChecker(new Checker(start_colours[(j+offset)%2][i]));
+                }
+
+    }
+
+    public void play()
+    {
+        while(!movement.checkWin())
+            movement.moveChecker();
+    }
+
+}
---
+++ Movement.java
@@ -0,0 +1,171 @@
+import java.util.ArrayList;
+
+public class Movement
+{
+    private int num_pips = 24;
+    private Command_Generator genCommands;
+
+    private ArrayList<Pip> Pips;
+    private ArrayList<Pip> Bar;
+
+    private int[] removedCheckers = {0,0};
+
+    private Dice dice;
+    private Turn turn;
+
+    private Command nextMove;
+
+    public Movement(Dice dice, ArrayList<Pip> pips, Turn turn, ArrayList<Pip> Bar)
+    {
+        this.dice = dice;
+        this.turn = turn;
+        this.Pips = pips;
+        this.Bar = Bar;
+        this.genCommands = new Command_Generator(dice, Pips, turn, Bar);
+    }
+
+    private void populateCommands()
+    {
+        dice.rollDice(true);
+        genCommands.processCommands();
+        genCommands.displayAllCommands();
+    }
+
+    private void updateCommands(Command move)
+    {
+        genCommands.updateCommands(move);
+        genCommands.displayAllCommands();
+    }
+
+    public boolean checkWin()
+    {
+        if(turn.returnTurn() == turn.returnOrientation())
+            return (removedCheckers[0] ==15) ;
+        else
+            return ( removedCheckers[1] == 15);
+    }
+
+    public void moveChecker()
+    {
+        Display();
+        populateCommands();
+        int total_steps = genCommands.returnTotalSteps();
+        //System.out.println("total is now: " + total_steps);
+
+        boolean turn_flag = (turn.returnTurn() != turn.returnOrientation());
+
+        while(total_steps > 0)
+        {
+            nextMove = genCommands.selectCommand();
+            //System.out.println("command" + nextMove.commandString());
+
+            if (nextMove != null)
+            {
+                int start = nextMove.returnStart();
+                int end = nextMove.returnEnd();
+
+                if(turn_flag && start != -1 )
+                    start = convert2AlterIndex(start);
+                if(turn_flag && end != -1 )
+                    end = convert2AlterIndex(end);
+
+
+                if(start ==-1 || !Pips.get(start).isEmpty() )
+                {
+                    Checker checker;
+                    if(start == -1)
+                    {
+                        if(!turn_flag)
+                            checker = Bar.getFirst().removeChecker();
+                        else
+                            checker = Bar.getLast().removeChecker();
+                    }
+                    else
+                        checker = Pips.get(start).removeChecker();
+
+                    if (checker != null)
+                    {
+                        if(end == -1)
+                        {
+                            if(turn.returnTurn() == turn.returnOrientation())
+                                removedCheckers[0] ++;
+                            else
+                                removedCheckers[1]++;
+                        }
+                        else
+                        {
+                            if(!Pips.get(end).isEmpty())
+                            {
+                                Pip check = Pips.get(end);
+                                if(check.returnColour() != turn.returnTurn())
+                                {
+                                    Checker oldCheck = check.removeChecker();
+                                    if (!turn_flag)
+                                        Bar.getLast().addChecker(oldCheck);
+                                    else
+                                        Bar.getFirst().addChecker(oldCheck);
+                                }
+                            }
+
+                            Pips.get(end).addChecker(checker);
+                            if (turn.returnTurn() == turn.returnOrientation())
+                                total_steps -= (((end - start) % num_pips) + num_pips) % num_pips;
+                            else
+                                total_steps -=  (((start-end) % num_pips) + num_pips) % num_pips;
+                           // System.out.println("Moved checker from pip " + start + " to pip " + (end));
+                            //System.out.println("total is now: " + total_steps);
+                        }
+
+                        if(total_steps > 0)
+                        {
+                            Display();
+                            updateCommands(nextMove);
+                        }
+                    }
+                    else
+                        System.out.println("No checker to move from pip " + start);
+                }
+            }
+            else
+                System.out.println("Error occurred when moving checkers. Please select again.");
+        }
+        dice.clearDice();
+        turn.changeTurn();
+    }
+
+    public int convert2AlterIndex(int i) { return (((num_pips-1-i)+num_pips)%num_pips); }
+
+    public void Display()
+    {
+        String output = " ";
+
+        if(turn.returnOrientation() == turn.returnTurn())
+        {
+            for (int i = (num_pips / 2); i < num_pips; i++)
+                output = output + Pips.get(i).Display() + ":" + i + " ";
+
+            output = output +"\n\n ";
+
+            for (int i = (num_pips / 2) - 1; i >= 0; i--)
+                output = output + Pips.get(i).Display() + ":"+ i + " ";
+
+            System.out.println(output);
+        }
+        else
+        {
+            for(int i = 0; i < (num_pips/2) ; i++)
+                output = output + Pips.get(i).Display() + ":" + convert2AlterIndex(i) + " ";
+
+            output = output + "\n\n ";
+
+            for (int i = num_pips-1; i > (num_pips / 2)-1 ; i--)
+                output = output + Pips.get(i).Display() + ":" + convert2AlterIndex(i) + " " ;
+
+            System.out.println(output);
+        }
+
+        System.out.println("Bar: First" + Bar.getFirst().returnLength() + "    Second: " + Bar.getLast().returnLength());
+
+    }
+
+}
