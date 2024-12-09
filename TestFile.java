cd90722edc41a09768f4d38f2adf45769a3b4958
Hannah Nolan <131493612+hnolan2019@users.noreply.github.com>
2024-12-09 05:09:05 +0000

    Add files via upload

---
+++ TestFile.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+import java.io.File;
+
+
+public class TestFile
+{
+    private String filename = "Command_Test.txt";
+    private Scanner input;
+
+    public TestFile()
+    {
+        try
+        {
+            File file = new File(filename);
+            input = new Scanner(file);
+        }
+        catch (Exception ex)
+        {
+            System.out.println("Error");
+        }
+
+    }
+
+    public String readNext()
+    {
+        try {
+            if (input.hasNextLine()) {
+                String line = input.nextLine();
+                return line;
+            }
+        }
+        catch(Exception ex) {
+            System.out.println("Couldn't find file 'Command_Test.txt' in your directory. Please ensure the file has the correct name.");
+
+        }
+        return null;
+    }
+
+    public void closeInput()
+    {
+        input.close();
+    }
+
+    public int nextInt()
+    {
+        int test;
+
+        try {
+            if (input.hasNextInt()) {
+                test = input.nextInt();
+                return test;
+            }
+        }
+        catch(Exception ex) {
+            System.out.println("Couldn't find entry.");
+
+        }
+        return 0;
+    }
+
+}
