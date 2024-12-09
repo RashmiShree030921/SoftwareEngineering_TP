49535cd8d533c500ea6d6c91b26b14a4a6b4ec56
Hannah Nolan <131493612+hnolan2019@users.noreply.github.com>
2024-12-09 00:39:36 +0000

    Update TurnTest.java

--- TurnTest.java
+++ TurnTest.java
@@ -4,62 +4,46 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TurnTest {
 
-    // Test for Turn class
-    // public turn
-    // public returnOrientation
-    // public returnTurn
-    // public returnColour
-    // public changeTurn
-
-    @org.junit.jupiter.api.Test
-    void check() {
-
         //Methods to test in TurnTest.java-------| is method testsed |-------
         // setTurn()                                    yes
         //changeTurn()                                  yes
         //returnTurn()                                  yes
     //returnOrientation()                              yes
 
+    //Test for setTurn
+    Turn turn;
 
-        // Test case for B  (true)
-        boolean BlackColour = true;
-        Turn BlackTurn = new Turn();
-        BlackTurn.setTurn(BlackColour, BlackColour);
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        turn = new Turn();
+    }
 
-        // Assert that the turn color matches the input - tests return turn method
-        assertEquals(BlackColour, BlackTurn.returnTurn(), "Turn should match the input boolean value");
+    @org.junit.jupiter.api.Test
+    void testSetTurn() {
+        turn.setTurn(true, true);
+        assertEquals(true, turn.returnTurn(), "Turn should match the input boolean value");
+        assertEquals(true, turn.returnOrientation(), "Orientation should match the input boolean value");
+    }
 
-        // Assert that the orientation color matches the input - tests return orientation method
-        assertEquals(BlackColour, BlackTurn.returnOrientation(), "Orientation should match the input boolean value");
+    @org.junit.jupiter.api.Test
+    void testReturnColourForBlack() {
+        turn.setTurn(true, true);
+        assertEquals("B", turn.returnColour(), "Turn should return 'B' for black (true)");
+    }
 
-        // Assert that the display string is "B" for black - tests return colour method
-        assertEquals("B", BlackTurn.returnColour(), "Turn should return 'B' for black (true)");
+    @org.junit.jupiter.api.Test
+    void testReturnColourForWhite() {
+        turn.setTurn(false, false);
+        assertEquals("W", turn.returnColour(), "Turn should return 'W' for white (false)");
+    }
 
-        // Test case for white color (false)
-        boolean whiteColour = false;
-        Turn whiteTurn = new Turn();
-        whiteTurn.setTurn(whiteColour, whiteColour);
-
-        // Assert that the turn color matches the input
-        assertEquals(whiteColour, whiteTurn.returnTurn(), "Turn should match the input boolean value");
-
-        // Assert that the orientation color matches the input
-        assertEquals(whiteColour, whiteTurn.returnOrientation(), "Orientation should match the input boolean value");
-
-        // Assert that the display string is "W" for white
-        assertEquals("W", whiteTurn.returnColour(), "Turn should return 'W' for white (false)");
-
-        // Test case for changing turn
-        BlackTurn.changeTurn();
-
-        // Assert that the turn color has changed
-        assertEquals(whiteColour, BlackTurn.returnTurn(), "Turn should change to the opposite of the input boolean value");
-
-        // Test case for changing turn
-        whiteTurn.changeTurn();
-
-        // Assert that the turn color has changed
-        assertEquals(BlackColour, whiteTurn.returnTurn(), "Turn should change to the opposite of the input boolean value");
+    @org.junit.jupiter.api.Test
+    void testChangeTurn() {
+        turn.setTurn(true, true);
+        turn.changeTurn();
+        assertEquals(false, turn.returnTurn(), "Turn should toggle to false after changeTurn");
+        turn.changeTurn();
+        assertEquals(true, turn.returnTurn(), "Turn should toggle back to true after another changeTurn");
     }
 
 }
