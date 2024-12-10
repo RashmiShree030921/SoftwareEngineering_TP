5177f4a093b185eb7c7bb293c13fdac44197e831
Hannah Nolan <131493612+hnolan2019@users.noreply.github.com>
2024-12-10 14:03:33 +0000

    Update CheckerTest.java

--- test files/CheckerTest.java
+++ test files/CheckerTest.java
@@ -12,19 +12,22 @@ class CheckerTest {
 
         // Test case for black colour - (true)
         boolean BlackColour = true; // set test boolean to true
-        Checker BlackChecker = new Checker(BlackColour); // create 
+        Checker BlackChecker = new Checker(BlackColour); // create checker object from test boolean
 
-        // Assert that the checker color matches the input - tests return colour method
+        // Assert that the checker colour matches the input - tests return colour method (true)
         assertEquals(BlackColour, BlackChecker.returnColour(), "Checker should match the input boolean value");
 
-        // Assert that the display string is "B" for brown - tests return string method
+        // Assert that the display string is "B" for blaack - tests return string method
         assertEquals("B", BlackChecker.returnString(), "Checker should return 'B' for black (true)");
+
+        // Test case for white color (false) - invalid
+        assertNotEquals(false, BlackChecker.returnColour(),"Checker should match the input boolean value");
 
         // Test case for white color (false)
         boolean whiteColour = false;
         Checker whiteChecker = new Checker(whiteColour);
 
-        // Assert that the checker color matches the input - tests return colour method
+        // Assert that the checker colour matches the input - tests return colour method
         assertEquals(whiteColour, whiteChecker.returnColour(), "Checker should match the input boolean value");
 
         // Assert that the display string is "W" for white -    tests return string method
